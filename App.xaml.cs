ff6999660bd4cb9bfa2476b206cef2f53bed0a7e
Nate Bross <nate@brosstribe.com>
2022-08-29 16:40:10 -0500

    add debugging config

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Windows;
+﻿using System.Windows;
 
-namespace WPFClock
+namespace WPFClock;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,65 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.ComponentModel;
 using System.Threading;
 
-namespace WPFClock
+namespace WPFClock;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window, INotifyPropertyChanged
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public String _TheCurrenTimeString;
+    public String TheCurrenTimeString
     {
-        public String _TheCurrenTimeString;
-        public String TheCurrenTimeString
-        {
-            get { return _TheCurrenTimeString; }
-            set { _TheCurrenTimeString = value; OnPropertyChanged("TheCurrenTimeString"); }
-        }
-
-        Timer tmr;
-        public MainWindow()
-        {
-            InitializeComponent();
-
-            this.DataContext = this;
-
-            tmr = new Timer(new TimerCallback((x) =>
-            {
-                TheCurrenTimeString = DateTime.Now.ToLongTimeString();
-            }));
-
-            tmr.Change(0, 500);
-        }
-
-
-        #region INotifyPropertyChanged Members
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        /// <summary>
-        /// Raises this object's PropertyChanged event.
-        /// </summary>
-        /// <param name="propertyName">The property that has a new value.</param>
-        protected void OnPropertyChanged(string propertyName)
-        {
-            PropertyChangedEventHandler handler = this.PropertyChanged;
-            if (handler != null)
-            {
-                var e = new PropertyChangedEventArgs(propertyName);
-                handler(this, e);
-            }
-        }
-        #endregion
+        get { return _TheCurrenTimeString; }
+        set { _TheCurrenTimeString = value; OnPropertyChanged("TheCurrenTimeString"); }
     }
+
+    Timer tmr;
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        this.DataContext = this;
+
+        tmr = new Timer(new TimerCallback((x) =>
+        {
+            TheCurrenTimeString = DateTime.Now.ToLongTimeString();
+        }));
+
+        tmr.Change(0, 500);
+    }
+
+
+    #region INotifyPropertyChanged Members
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    /// <summary>
+    /// Raises this object's PropertyChanged event.
+    /// </summary>
+    /// <param name="propertyName">The property that has a new value.</param>
+    protected void OnPropertyChanged(string propertyName)
+    {
+        PropertyChangedEventHandler handler = this.PropertyChanged;
+        if (handler != null)
+        {
+            var e = new PropertyChangedEventArgs(propertyName);
+            handler(this, e);
+        }
+    }
+    #endregion
 }
